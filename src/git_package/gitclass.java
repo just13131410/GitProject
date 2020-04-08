b64fedbd4c4e08c3ee5bedf0c77f298e7b58b4ae
just13131410 <just-k@gmx.net>
2020-04-08 17:00:53 +0200

    Java Git Projekt Änderung

--- src/git_package/gitclass.java
+++ src/git_package/gitclass.java
@@ -2,10 +2,7 @@ package git_package;
 
 public class gitclass {
 	public static void main(String args[]){  
-	     System.out.println("Hello Java");  
-	     System.out.println("Hello GitHub");  
-	     System.out.println("Hello GitHub2");  
-	     System.out.println("Hello GitHub2");  
+	     System.out.println("new Project");  
 	 }  
 
 }
