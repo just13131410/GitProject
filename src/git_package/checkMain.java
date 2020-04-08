26e3c6de5bf81ff61d446ac1c8edf718afe16d10
just13131410 <just-k@gmx.net>
2020-04-08 13:25:33 +0200

    Java Git Projekt

---
+++ src/git_package/checkMain.java
@@ -0,0 +1,14 @@
+package git_package;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class checkMain {
+
+	@Test
+	void testMain() {
+		//fail("Not yet implemented");
+	}
+
+}
---
+++ src/git_package/gitclass.java
@@ -0,0 +1,8 @@
+package git_package;
+
+public class gitclass {
+	public static void main(String args[]){  
+	     System.out.println("Hello Java");  
+	 }  
+
+}
